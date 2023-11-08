ae2afb772aa5cbbe9b91e5967750b55a817de234
Hamidin <mailnidimah.hamidin@gmail.com>
2023-11-08 11:07:09 +0700

    Format Export Ticket

--- app/Exports/TicketsExport.php
+++ app/Exports/TicketsExport.php
@@ -8,8 +8,11 @@ use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Contracts\Support\Responsable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class TicketsExport implements FromQuery, Responsable, WithHeadings, ShouldAutoSize
+class TicketsExport implements FromQuery, Responsable, WithHeadings, ShouldAutoSize, withMapping, WithColumnFormatting
 {
     use Exportable;
     private $fileName = 'ticket.xlsx';
@@ -26,7 +29,7 @@ class TicketsExport implements FromQuery, Responsable, WithHeadings, ShouldAutoS
     public function headings(): array
     {
         return [
-            '#',
+            // '#',
             'MSG ID', // From wa baileys
             'Ticket Number',
             'Phone Number',
@@ -38,6 +41,35 @@ class TicketsExport implements FromQuery, Responsable, WithHeadings, ShouldAutoS
             'PIC Assign Time',
             'Rating',
             'Category',
+            'Created At',
+            'Updated At',
+        ];
+    }
+
+    public function map($ticket): array
+    {
+        return [
+            $ticket->msg_id,
+            $ticket->ticket_number,
+            "'".$ticket->phone_number,
+            $ticket->push_name,
+            $ticket->start_time,
+            $ticket->end_time,
+            $ticket->status,
+            $ticket->pic,
+            $ticket->pic_time,
+            $ticket->rate,
+            $ticket->category,
+            Date::dateTimeToExcel($ticket->created_at),
+            Date::dateTimeToExcel($ticket->updated_at),
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'L' => NumberFormat::FORMAT_DATE,
+            'M' => NumberFormat::FORMAT_DATE,
         ];
     }
 }
